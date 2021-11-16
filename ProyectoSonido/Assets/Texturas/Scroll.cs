51467dd8b55972702b178ae3cd8444e6f7816293
Tatiana <tatiandu@ucm.es>
2021-11-16 12:56:06 +0100

    Subida inicial

---
+++ ProyectoSonido/Assets/Texturas/Scroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    public Vector2 uvAnimationRate;
+
+    Vector2 uvOffset;
+    Renderer renderer_;
+
+
+    void Start()
+    {
+        uvOffset = Vector2.zero;
+        renderer_ = GetComponent<Renderer>();
+    }
+
+    void LateUpdate()
+    {
+        uvOffset += (uvAnimationRate * Time.deltaTime);
+        if (renderer_.enabled)
+        {
+            renderer_.materials[0].SetTextureOffset("_MainTex", uvOffset);
+            //renderer_.materials[materialIndex].SetTextureOffset("_BumpMap", uvOffset);
+        }
+    }
+}
